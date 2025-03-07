3a803f3539f3315c9155a89406af246724e8e86e
Marion <lamacq.marion@hotmail.fr>
2025-03-07 17:09:03 +0100

    LM: Commit intermédiaire

--- src/main/java/com/openclassrooms/payMyBuddy/security/SpringSecurityConfig.java
+++ src/main/java/com/openclassrooms/payMyBuddy/security/SpringSecurityConfig.java
@@ -1,6 +1,5 @@
-package com.openclassrooms.payMyBuddy.config;
+package com.openclassrooms.payMyBuddy.security;
 
-import com.openclassrooms.payMyBuddy.security.CustomUserDetailsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
